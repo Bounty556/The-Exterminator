1abf975be0697a6530b8c00c2070dbb70138c634
Jacob Peterson <jpeterso1@berkadia.com>
2021-07-08 16:27:38 -0600

    Started create-project route logic

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "create-project",
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.css']
 })
-export class CreateProjectComponent {
-  
+export class CreateProjectComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      projectName: this.formBuilder.control(''),
+      projectDescription: this.formBuilder.control(''),
+      privacyMode: this.formBuilder.control('Public'),
+      projectManagers: this.formBuilder.control('')
+    });
+  }
 }
