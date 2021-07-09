f286b9c8e0297edaf099c388a7942c8e69470ecb
Jacob Peterson <jpeterso1@berkadia.com>
2021-07-09 15:09:15 -0600

    Added forms for my-account route

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,8 +1,58 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'ex-my-account',
   templateUrl: './my-account.component.html',
   styleUrls: ['./my-account.component.css']
 })
-export class MyAccountComponent {}
+export class MyAccountComponent implements OnInit {
+  changeUsernameForm: FormGroup;
+  changePasswordForm: FormGroup;
+  changeEmailForm: FormGroup;
+  dangerZoneForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+  
+  ngOnInit() {
+    this.changeUsernameForm = this.formBuilder.group({
+      newUsername: this.formBuilder.control(''),
+      newUsernamePassword: this.formBuilder.control('')
+    });
+
+    this.changePasswordForm = this.formBuilder.group({
+      newPasswordCurrent: this.formBuilder.control(''),
+      newPassword: this.formBuilder.control(''),
+      newPasswordConfirm: this.formBuilder.control('')
+    });
+
+    this.changeEmailForm = this.formBuilder.group({
+      newEmail: this.formBuilder.control(''),
+      newEmailPassword: this.formBuilder.control('')
+    });
+
+    this.dangerZoneForm = this.formBuilder.group({
+      dangerZonePassword: this.formBuilder.control('')
+    });
+  }
+
+  changeUsername(form) {
+
+  }
+
+  changePassword(form) {
+
+  }
+
+  changeEmail(form) {
+
+  }
+
+  deleteAccount(form) {
+
+  }
+
+  removeAllProjects(form) {
+
+  }
+}
--- src/app/my-account/my-account.module.ts
+++ src/app/my-account/my-account.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { myAccountRouting } from "./my-account.routing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MyAccountComponent } from "./my-account.component";
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     myAccountRouting
+  ],
+  declarations: [
+    MyAccountComponent
   ]
 })
 export class MyAccountModule {}
