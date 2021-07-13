cab375c50af4a1d0569af506831549e1eb99610b
Jacob Peterson <jpeterso1@berkadia.com>
2021-07-13 08:09:18 -0600

    Added html to project-bug page

--- src/app/project/project-bug-component.ts
+++ src/app/project/project-bug-component.ts
@@ -1,8 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'ex-project-bug',
   templateUrl: './project-bug-component.html',
   styleUrls: ['./project-bug-component.css']
 })
-export class ProjectBugComponent {}
+export class ProjectBugComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+  
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      responseText: this.formBuilder.control(''),
+      bugStatus: this.formBuilder.control('')
+    });
+  }
+
+  onSubmit(form) {
+  }
+}
